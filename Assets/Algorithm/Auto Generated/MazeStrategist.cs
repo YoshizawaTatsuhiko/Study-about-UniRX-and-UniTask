4fa5849764c8298218bec0d34f5ee41047e0bf6c
tatsupiko <identity2022@outlook.jp>
2023-05-01 23:57:54 +0900

    MazeStrategistクラスに「配列を2次元配列に変換する関数」を追加した。

--- Assets/Algorithm/Auto Generated/MazeStrategist.cs
+++ Assets/Algorithm/Auto Generated/MazeStrategist.cs
@@ -12,15 +12,32 @@ public class MazeStrategist
         _strategy = strategy;
     }
 
-    public void GenerateMaze(int w, int h)
+    /// <summary>設計図から迷路を作成する</summary>
+    public void AssembleFromBlueprint(int width, int height)
     {
         Debug.Log($"{_strategy?.GetType()}を実行します。");
-        _strategy.ExecuteBlueprint(w, h);
+        string[] strArray = _strategy.CreateBlueprint(width, height).Split("\n");
+        string[,] mazeInfo = new string[strArray[0].Length, strArray.Length];
     }
 
-    public void ChangeStrategy(IMazeStrategy strategy)
+    /// <summary>自動生成アルゴリズムを変更する</summary>
+    public void ChangeTheAssembly(IMazeStrategy strategy)
     {
-        Debug.Log($"{strategy}に切り替えるまで、3 2 1...");
+        Debug.Log($"{_strategy} => {strategy}に切り替えるまで、3 2 1...");
         _strategy = strategy;
+    }
+
+    private string[,] To2DArray(string[] array)
+    {
+        string[,] twoDimensionalArray = new string[array[0].Length, array.Length];
+
+        for (int i = 0; i < twoDimensionalArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < twoDimensionalArray.GetLength(1); j++)
+            {
+                twoDimensionalArray[i, j] = array[i][j].ToString();
+            }
+        }
+        return twoDimensionalArray;
     }
 }
