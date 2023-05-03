e094a8f8f60d55307e2f19126c3993362f2f13e9
tatsupiko <identity2022@outlook.jp>
2023-05-03 23:56:22 +0900

    MazeGeneratorクラスに「アルゴリズムに応じて迷路生成の仕方を変更する」関数を追加した。（作成中）

--- Assets/Algorithm/Auto Generated/MazeGenerator.cs
+++ Assets/Algorithm/Auto Generated/MazeGenerator.cs
@@ -7,10 +7,46 @@ public class MazeGenerator : MonoBehaviour
 {
     [SerializeField] private int _width = 5;
     [SerializeField] private int _height = 5;
-    MazeStrategist _autoAlgorithm = new MazeStrategist(new WallExtendAlgorithm());
+    [SerializeField] private SelectAlgorithm _algorithm = SelectAlgorithm.None;
+    MazeStrategist _autoAlgorithm = new(new WallExtendAlgorithm());
 
     private void Start()
     {
-        _autoAlgorithm.AssembleFromBlueprint(_width, _height);
+        GenerateMaze(_algorithm);
+    }
+
+    private void GenerateMaze(SelectAlgorithm algorithm)
+    {
+        switch (algorithm)
+        {
+            case SelectAlgorithm.WallExtend:
+                _autoAlgorithm.ChangeTheAssembly(new WallExtendAlgorithm());
+                break;
+            default:
+                break;
+        }
+        string[,] string2DArray = _autoAlgorithm.AssembleFromBlueprint(_width, _height);
+
+        for (int i = 0; i < string2DArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < string2DArray.GetLength(1); j++)
+            {
+                string chara = string2DArray[i, j];
+
+                switch (chara)
+                {
+                    case "W":
+                        break;
+                    case "F":
+                        break;
+                }
+            }
+        }
+    }
+
+    private enum SelectAlgorithm
+    {
+        None,
+        WallExtend,
     }
 }
