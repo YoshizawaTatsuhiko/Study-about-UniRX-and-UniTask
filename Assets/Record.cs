5ef9db9b95a5f111e33360ceafe2878fb4fecd91
tatsupiko <identity2022@outlook.jp>
2023-12-26 22:26:26 +0900

    [add] Objectの座標と回転を記録する構造体を作成した。

---
+++ Assets/Record.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public struct Record
+{
+    public Vector3 Position;
+    public Quaternion Rotation;
+
+    public Record(Vector3 position, Quaternion rotation)
+    {
+        Position = position;
+        Rotation = rotation;
+    }
+}
