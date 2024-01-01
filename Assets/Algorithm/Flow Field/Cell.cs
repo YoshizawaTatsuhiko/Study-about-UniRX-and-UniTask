9af2aedd8e547756e91b8d441770dd7bbd2d501d
tatsupiko <identity2022@outlook.jp>
2024-01-02 02:48:43 +0900

    [update] public変数をプロパティとメンバー変数に分離した。

--- Assets/Algorithm/Flow Field/Cell.cs
+++ Assets/Algorithm/Flow Field/Cell.cs
@@ -5,24 +5,28 @@ namespace Learning.Algorithm.FlowField
 {
     public class Cell
     {
-        public Vector3 _worldPos = Vector3.zero;
-        public Vector2Int _gridIndex = Vector2Int.zero;
-        //public byte _cost;
+        public Vector3 WorldPos => _worldPos;
+        public Vector2Int GridIndex => _gridIndex;
+        public byte Cost => _cost;
         //public ushort _bestCost;
         //public GridDirection _bestDirection;
+
+        private Vector3 _worldPos = Vector3.zero;
+        private Vector2Int _gridIndex = Vector2Int.zero;
+        private byte _cost = 0;
 
         public Cell(Vector3 worldPos, Vector2Int gridIndex)
         {
             _worldPos = worldPos;
             _gridIndex = gridIndex;
-            //_cost = 1;
+            _cost = 1;
         }
 
-        //public void IncreaseCost(byte amount)
-        //{
-        //    if (_cost == byte.MaxValue) return;
+        public void IncreaseCost(int amount)
+        {
+            if (_cost == byte.MaxValue) return;
 
-        //    _cost = (byte)Mathf.Min(_cost + amount, byte.MaxValue);
-        //}
+            _cost = (byte)Mathf.Min(_cost + amount, byte.MaxValue);
+        }
     }
 }
