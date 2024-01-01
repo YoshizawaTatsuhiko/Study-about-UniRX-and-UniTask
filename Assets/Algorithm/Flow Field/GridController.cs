b4e38e926724ea25c136ce6b30aec623d2f6d750
tatsupiko <identity2022@outlook.jp>
2024-01-02 02:53:22 +0900

    [update] public変数をプロパティとメンバー変数に分離した。

--- Assets/Algorithm/Flow Field/GridController.cs
+++ Assets/Algorithm/Flow Field/GridController.cs
@@ -5,32 +5,36 @@ namespace Learning.Algorithm.FlowField
 {
     public class GridController : MonoBehaviour
     {
-        public Vector2Int gridSize;
-        public float cellRadius = 0.5f;
-        public FlowField curFlowField;
-        //public GridDebug gridDebug;
+        public Vector2Int GridSize => _gridSize;
+        public float CellRadius => _cellRadius;
+
+        [SerializeField] private Vector2Int _gridSize = Vector2Int.zero;
+        [SerializeField] private float _cellRadius = 0.5f;
+        [SerializeField] private GridDebug gridDebug = null;
+
+        private FlowField _curFlowField = null;
 
         private void InitializeFlowField()
         {
-            curFlowField = new FlowField(cellRadius, gridSize);
-            curFlowField.CreateGrid();
-            //gridDebug.SetFlowField(curFlowField);
+            _curFlowField = new FlowField(_cellRadius, _gridSize);
+            _curFlowField.CreateGrid();
+            gridDebug.SetFlowField(_curFlowField);
         }
 
         private void Update()
         {
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0)) // LeftClick
             {
                 InitializeFlowField();
 
-                //curFlowField.CreateCostField();
+                _curFlowField.CreateCostField();
 
                 //Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f);
                 //Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(mousePos);
                 //Cell destinationCell = curFlowField.GetCellFromWorldPos(worldMousePos);
                 //curFlowField.CreateIntegrationField(destinationCell);
 
-                //curFlowField.CreateFlowField();
+                //_curFlowField.CreateFlowField();
 
                 //gridDebug.DrawFlowField();
             }
