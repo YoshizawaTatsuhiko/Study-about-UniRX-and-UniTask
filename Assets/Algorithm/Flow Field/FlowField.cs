0c9ace355dfd68d320940c911f531c69b9aca46c
tatsupiko <identity2022@outlook.jp>
2024-01-02 02:52:06 +0900

    [update] Gridを作成する際、Grid全体のWorld座標が中心になるように修正した。 [update] CostFIeldの作成まで学習を進めた。

--- Assets/Algorithm/Flow Field/FlowField.cs
+++ Assets/Algorithm/Flow Field/FlowField.cs
@@ -23,41 +23,44 @@ namespace Learning.Algorithm.FlowField
 
         public void CreateGrid()
         {
-            Grid = new Cell[GridSize.x, GridSize.y];
+            Grid = new Cell[GridSize.y, GridSize.x];
 
-            for (int x = 0; x < GridSize.x; x++)
+            for (int y = 0; y < GridSize.y; y++)
             {
-                for (int y = 0; y < GridSize.y; y++)
+                for (int x = 0; x < GridSize.x; x++)
                 {
-                    Vector3 worldPos = new Vector3(_cellDiameter * x + CellRadius, 0, _cellDiameter * y + CellRadius);
-                    Grid[x, y] = new Cell(worldPos, new Vector2Int(x, y));
+                    Vector3 worldPos = new Vector3(_cellDiameter * x + CellRadius, 0, _cellDiameter * -y - CellRadius);
+                    worldPos += new Vector3(GridSize.x / -2f, 0f, GridSize.y / 2f);
+                    Grid[y, x] = new Cell(worldPos, new Vector2Int(y, x));
                 }
             }
         }
 
-        //public void CreateCostField()
-        //{
-        //    Vector3 cellHalfExtents = Vector3.one * cellRadius;
-        //    int terrainMask = LayerMask.GetMask("Impassible", "RoughTerrain");
-        //    foreach (Cell curCell in grid)
-        //    {
-        //        Collider[] obstacles = Physics.OverlapBox(curCell.worldPos, cellHalfExtents, Quaternion.identity, terrainMask);
-        //        bool hasIncreasedCost = false;
-        //        foreach (Collider col in obstacles)
-        //        {
-        //            if (col.gameObject.layer == 8)
-        //            {
-        //                curCell.IncreaseCost(255);
-        //                continue;
-        //            }
-        //            else if (!hasIncreasedCost && col.gameObject.layer == 9)
-        //            {
-        //                curCell.IncreaseCost(3);
-        //                hasIncreasedCost = true;
-        //            }
-        //        }
-        //    }
-        //}
+        public void CreateCostField()
+        {
+            Vector3 cellHalfExtents = Vector3.one * CellRadius;
+            int terrainMask = LayerMask.GetMask("Impassible", "RoughTerrain");
+
+            foreach (Cell curCell in Grid)
+            {
+                Collider[] obstacles = Physics.OverlapBox(curCell.WorldPos, cellHalfExtents, Quaternion.identity, terrainMask);
+                bool hasIncreasedCost = false;
+
+                foreach (Collider col in obstacles)
+                {
+                    if (col.gameObject.layer == 6)
+                    {
+                        curCell.IncreaseCost(255);
+                        continue;
+                    }
+                    else if (!hasIncreasedCost && col.gameObject.layer == 7)
+                    {
+                        curCell.IncreaseCost(3);
+                        hasIncreasedCost = true;
+                    }
+                }
+            }
+        }
 
         //public void CreateIntegrationField(Cell _destinationCell)
         //{
